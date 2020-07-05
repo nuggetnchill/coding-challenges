68fb0707dc8cdfcfc911d12d6d134a4b653e387f
Justin Nguyen <nuggetnchill@gmail.com>
2020-07-05 16:14:34 -0500

    part2

--- advent of code 2015/day 2/README.md
+++ advent of code 2015/day 2/README.md
@@ -1,3 +1,4 @@
+# Part 1:
 
 The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
 
@@ -13,6 +14,22 @@ All numbers in the elves' list are in feet.
 
 How many total square feet of wrapping paper should they order?
 
+
+# Part 2:
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+- A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+- A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+
+How many total feet of ribbon should they order?
+
 ### Comment:
-    I was able to dip my toe further into iterating through arrays and functions for array such as .map() and .reduce()
-    also learned something about geometry and wrapping present :D
+    I was able to dip my toe further into iterating through arrays and understand more about functions for array such as .split(), .map() and .reduce()
+    Learned something about geometry and wrapping present :D
+    Also it was alot of back and forth of checking to see if my result is a string or a number, alot of NaN results.
+    Part 2 I tried a different approach. 
+    At the end I feel more confident in working with arrays as well as multiple layers of array.    
--- advent of code 2015/day 2/solution.js
+++ advent of code 2015/day 2/solution.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
-function question() {
+// Part 1
+function question1() {
     fs.readFile('./input.txt', (err, data) => {
         const dimensionList = data.toString();
         const dimensionS = dimensionList.split('\n'); //split into multiple strings
-        const dimension = dimensionS.map(array => array.trim()); // trim \r
+        const dimension = dimensionS.map(array => array.trim()); // trim \r (later found out i don't have to trim \r)
         const lwh = dimension.map(array => array.split('x'));  // remove x and split into multiple arrays
         
         // calculate SURFACE AREA of each box and return an array
@@ -17,18 +18,13 @@ function question() {
         const slack = lwh.map((a) => {
             let smallest =  Math.min( (a[0] * a[1]), (a[0] * a[2]), (a[1] * a[2]));
             return smallest;
-        }
-        )
+        })
         
         // Total SURFACE AREA
-        const totalSA = boxes.reduce((acc,val) => {
-            return acc + val;
-        })
+        const totalSA = boxes.reduce((acc,val) => acc + val)
 
         //Total SLACK AREA
-        const totalSlack = slack.reduce((acc,val) => {
-            return acc + val;
-        })
+        const totalSlack = slack.reduce((acc,val) => acc + val)
 
         // Log final answer
         console.log(totalSA + totalSlack)
@@ -36,5 +32,27 @@ function question() {
     })
 }
 
-question()
+question1()
 
+// Part 2
+
+function question2() {
+    fs.readFile('./input.txt', (err, data) => {
+        const input = data.toString().split('\n')
+        const result = input.reduce((total, _lwh) => {
+            const lwh = _lwh.split('x')
+            const length = parseInt(lwh[0]);
+            const width = parseInt(lwh[1]);
+            const height = parseInt(lwh[2]);
+            const max =  Math.max(length, width,height) *  2 ;
+            
+            return total 
+                    + ( length + length + width + width + height + height - Math.max(length, width,height) - Math.max(length, width,height)  )
+                    + ( length * width * height ) 
+              
+        },0);
+            console.log(result);
+        })
+}
+
+question2();
