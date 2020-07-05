5c74dee0ec131813bbdc962689523f26ef94e855
Justin Nguyen <nuggetnchill@gmail.com>
2020-07-05 14:54:56 -0500

    finalize

--- advent of code 2015/day 2/solution.js
+++ advent of code 2015/day 2/solution.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
 
-
 function question() {
     fs.readFile('./input.txt', (err, data) => {
         const dimensionList = data.toString();
-        const dimensionS = dimensionList.split('\n');
-        const dimension = dimensionS.map(array => array.trim());
-        // const replaced = dimension.map(array => array.replace(/\x/g,'*')); // replacing x with * (probably don't need this)
-        const lwh = dimension.map(array => array.split('x'));
+        const dimensionS = dimensionList.split('\n'); //split into multiple strings
+        const dimension = dimensionS.map(array => array.trim()); // trim \r
+        const lwh = dimension.map(array => array.split('x'));  // remove x and split into multiple arrays
+        
+        // calculate SURFACE AREA of each box and return an array
+        const boxes = lwh.map( (a) => {
+            let surfaceArea = (a[0]*a[1]*2) + (a[0]*a[2]*2) + (a[1]*a[2]*2)
+            return surfaceArea
+            })
+            
+        // area of smallest side for slack
+        const slack = lwh.map((a) => {
+            let smallest =  Math.min( (a[0] * a[1]), (a[0] * a[2]), (a[1] * a[2]));
+            return smallest;
+        }
+        )
+        
+        // Total SURFACE AREA
+        const totalSA = boxes.reduce((acc,val) => {
+            return acc + val;
+        })
 
-        console.log(dimensionList)
-        console.log(lwh);
+        //Total SLACK AREA
+        const totalSlack = slack.reduce((acc,val) => {
+            return acc + val;
+        })
+
+        // Log final answer
+        console.log(totalSA + totalSlack)
+
     })
 }
 
